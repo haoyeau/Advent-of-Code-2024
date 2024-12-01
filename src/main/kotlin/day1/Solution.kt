3f8b9fc2ed1793b6ee7cb34afd67522a29420b35
Hao Ye <haoye.au@gmail.com>
2024-12-01 19:28:49 +1100

    Resolve day1-part2
    Calculate similarity score

--- src/main/kotlin/day1/Solution.kt
+++ src/main/kotlin/day1/Solution.kt
@@ -6,14 +6,8 @@ import kotlin.math.abs
 object Solution {
 
     fun calculateDistanceBetweenList(): Int{
-        val leftList = mutableListOf<Int>()
-        val rightList = mutableListOf<Int>()
         // read input
-        File("src/main/kotlin/day1/input.txt").forEachLine { line ->
-            val numbers = line.split("\\s+".toRegex())
-            leftList.add(numbers[0].toInt())
-            rightList.add(numbers[1].toInt())
-        }
+        val (leftList, rightList) = readList()
         // sort list
         leftList.sort()
         rightList.sort()
@@ -21,6 +15,27 @@ object Solution {
         return leftList.zip(rightList).fold(0){
             acc, pair -> acc + abs(pair.first - pair.second)
         }
+    }
+
+    fun calculateSimilarityScore(): Int{
+        // read input
+        val (leftList, rightList) = readList()
+        // accumulate score
+        return leftList.fold(0){
+            acc, i -> acc + i*rightList.count { it == i }
+        }
+    }
+
+    private fun readList(): Pair<MutableList<Int>, MutableList<Int>>{
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+        // parse input file
+        File("src/main/kotlin/day1/input.txt").forEachLine { line ->
+            val numbers = line.split("\\s+".toRegex())
+            leftList.add(numbers[0].toInt())
+            rightList.add(numbers[1].toInt())
+        }
+        return (leftList to rightList)
     }
 
 }
