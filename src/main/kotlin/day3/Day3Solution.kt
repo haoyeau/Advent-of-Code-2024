cda874db8fa836f1e414b3b42512d7b72ce15236
Hao Ye <haoye.au@gmail.com>
2024-12-03 16:38:05 +1100

    Resolve day3 part2
    Enable mult conditions

--- src/main/kotlin/day3/Day3Solution.kt
+++ src/main/kotlin/day3/Day3Solution.kt
@@ -13,4 +13,27 @@ object Day3Solution {
         }
     }
 
+    // part2
+    fun addUpEnabledInstructions(): Int{
+        val memoryContent = File("src/main/kotlin/day3/input.txt").readText()
+        var enabled = true
+        return Regex("""mul\((\d+),(\d+)\)|do\(\)|don't\(\)""").findAll(memoryContent).fold(0){
+            acc, matchResult ->
+            when (matchResult.value){
+                "do()" -> {
+                    enabled = true
+                    acc
+                }
+                "don't()" -> {
+                    enabled = false
+                    acc
+                }
+                else -> {
+                    if (enabled) acc + matchResult.groupValues[1].toInt()*matchResult.groupValues[2].toInt()
+                    else acc
+                }
+            }
+        }
+    }
+
 }
